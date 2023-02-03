cc69e128fe4404d47d7ba9d9e8a4a53f13397e3a
Laksito Pamilih <laksito@protonmail.com>
2023-02-03 13:20:14 +0700

    create photos page

---
+++ pages/users/_userId/albums/_albumId.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        v-for="photo in photos"
+        :key="photo.id"
+        xs="12"
+        sm="4"
+        md="3"
+        lg="2"
+      >
+        <v-img
+          :src="photo.thumbnailUrl"
+        />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'UserPage',
+  async asyncData ({ $axios, params }) {
+    const photos = await $axios.$get(`/albums/${params.albumId}/photos`)
+
+    return {
+      photos
+    }
+  }
+}
+</script>
